df47ceea572cc32d48be5bfe08f55d69f3f7dde5
roman <developerromankhan@gmail.com>
2024-03-04 03:30:37 +0500

    assignment completed

---
+++ app.js
@@ -0,0 +1,45 @@
+// heading started
+document.write("<h1>Hello <span style='color: green;'>JavaScript</span> Planet</h1>");
+console.log("Hello JavaScript Planet");
+// heading completed
+
+// paragraph-text started
+document.write("<p>I am the <b>master</b> of my own fate.<br><br>I am the <b>champion</b> of my own soule.<br><br>I am the <b>superhero</b> of my own life.</p>")
+console.log("I am the master of my own fate.\n\nI am the champion of my own soul.\n\nI am the superhero of my own life.")
+// paragraph-text completed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
